e7d2d02aa0472af4056851216aa05393bb52c5cf
Eduardo Chavez Barreto <eduardochvzb@localhost.localdomain>
2020-06-23 20:33:02 -0500

    Zoom y hover

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 import getJSON from './getJSON.js'
 
-const url = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json'
+const url = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'
 
 const svg = d3.select('svg');
 const height = svg.attr('height');
 const width = svg.attr('width');
 
+const world = svg.append('g');
+
+svg.call(d3.zoom()
+    .on('zoom',()=>{
+        world.attr('transform', d3.event.transform);
+    }));
+
 getJSON(url)
     .then(data =>{
         const countries = topojson.feature(data, data.objects.countries);
-        const projection = d3.geoEqualEarth().scale(1).fitSize([width,height],countries);
-        const pathGenerator = d3.geoPath().projection(projection);
-        
-        svg.append('path')
-            .attr('d', pathGenerator({type:'Sphere'}))
-            .attr('class','sphere')
 
-        svg.selectAll('path').data(countries.features)
+        const projection = d3.geoNaturalEarth1().scale(1).fitSize([width,height],countries);
+
+        const pathGenerator = d3.geoPath().projection(projection);
+        world.append('path')
+            .attr('d', pathGenerator({type:'Sphere'}))
+            .attr('class','sphere');
+
+        world.selectAll('path').data(countries.features)
         .enter()
             .append('path')
             .attr('d', pathGenerator)
-            .attr('class','country');
+            .attr('class','country')
+            .append('title')
+                .text(d => d.properties.name);
     })
 
     .catch(err => console.error(err));
